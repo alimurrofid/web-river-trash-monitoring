35676e0a6d633be287fe6435f653b984a7012fd1
alimurrofid <alimurrofid77@gmail.com>
2025-06-09 17:10:02 +0700

    feat:backend gitignore,seeder remove:disk

---
+++ Backend/src/drizzle/seed.ts
@@ -0,0 +1,23 @@
+import { db } from "../drizzle/db.ts"; // Sesuaikan path jika berbeda
+import { users } from "../drizzle/schema.ts"; // Sesuaikan path
+import { hash } from "bcryptjs";
+
+async function seedUsers() {
+  try {
+    // Enkripsi password
+    const hashedPassword = await hash("admin", 10); // 10 adalah salt rounds
+
+    await db.insert(users).values([
+      {
+        email: "admin@gmail.com",
+        password: hashedPassword,
+      },
+    ]);
+
+    console.log("✅ User seeding selesai.");
+  } catch (err) {
+    console.error("❌ Error saat seeding users:", err);
+  }
+}
+
+seedUsers();
